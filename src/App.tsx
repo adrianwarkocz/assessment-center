3a0d0141c27f75e5a26d9af0181102001dcf08b6
Aleksander <xxrtgxx333@gmail.com>
2022-04-25 13:17:29 +0200

    detail modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ActionBar, DetailsModal, Header, Table } from './components';
 import { Container } from './styles';
 
 export const App = () => {
-  const isDetailsModalOpen = false;
-  const isAddNewDetailsModalOpen = false;
+  const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false)
+  const [isAddNewDetailsModalOpen, seIsAddNewDetailsModalOpen] = useState(false);
+
+  function closeDetailPage() {
+    setIsDetailsModalOpen(false)
+  }
+
   return (
     <Container id="main-container">
       <Header />
       <ActionBar />
       <Table />
-      {isDetailsModalOpen && <DetailsModal />}
-      {isAddNewDetailsModalOpen && <DetailsModal />}
+      {isDetailsModalOpen && <DetailsModal  closeDetailPage={closeDetailPage} name='123' id={1} description='lorem ipsum' weight={3} price={5} />}
+      {isAddNewDetailsModalOpen && <DetailsModal closeDetailPage={closeDetailPage} name='123' id={1} description='lorem ipsum' weight={3} price={5} />}
+      <button onClick={()=>{setIsDetailsModalOpen(true)}}>test</button>
     </Container>
   );
 }
